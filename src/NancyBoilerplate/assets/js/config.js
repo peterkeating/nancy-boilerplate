b602c826aac6a55c7442a86a06ae2ec33d0cf106
Peter Keating <peter@moov2.com>
2013-09-24 09:41:53 +0100

    setup the basic JavaScript functionality which includes AMD loading via requireJS & the latest jQuery (1.10.2).

---
+++ src/NancyBoilerplate/assets/js/config.js
@@ -0,0 +1,17 @@
+/*global require*/
+require.config({
+
+   /**
+    * Array of dependencies that should be loaded as soon as require is defined.
+    * Currently this will load main.js which will trigger the loading of its
+    * dependencies.
+    */
+    deps: [ 'main' ],
+
+   /**
+    * Setting up root paths for module IDs.
+    */
+    paths: {
+        jquery: 'vendor/jquery-1.10.2'
+    },
+});
---
+++ src/NancyBoilerplate/assets/js/main.js
@@ -0,0 +1,8 @@
+/*global require */
+require([
+    'jquery'
+],
+
+function ($) {
+    // application entry code here...
+});
