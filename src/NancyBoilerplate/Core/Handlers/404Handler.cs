89ec1f664281cda81a25a2fee5fc56504963fab3
Peter Keating <peter@moov2.com>
2013-12-06 15:47:01 +0000

    added custom 404 page.

---
+++ src/NancyBoilerplate/Core/Handlers/404Handler.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+using System;
+
+namespace NancyBoilerplate.Core.Handlers
+{
+    public class _404Handler : DefaultViewRenderer, IStatusCodeHandler
+    {
+        public _404Handler(IViewFactory factory) : base(factory) { }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = RenderView(context, "404");
+            response.StatusCode = HttpStatusCode.NotFound;
+            context.Response = response;
+        }
+    }
+}
