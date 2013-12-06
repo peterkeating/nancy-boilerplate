3bed915398e0017c3c31bd0831a562d1831888ee
Peter Keating <peter@moov2.com>
2013-12-06 15:50:52 +0000

    added custom 500 error page.

---
+++ src/NancyBoilerplate/Core/Handlers/500Handler.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+using System;
+
+namespace NancyBoilerplate.Core.Handlers
+{
+    public class _500Handler : DefaultViewRenderer, IStatusCodeHandler
+    {
+        public _500Handler(IViewFactory factory) : base(factory) { }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.InternalServerError;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var response = RenderView(context, "500");
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            context.Response = response;
+        }
+    }
+}
